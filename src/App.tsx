8695cd1108fc4b6bb4ce8aa7ebf86ee001c7e314
HaoLong4935 <longsachse@gmail.com>
2024-10-10 14:19:11 +0700

    First init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Label } from './components/ui/label'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Label>Kekeke</Label>
+    </>
+  )
+}
+
+export default App
